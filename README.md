901897801e9e56881a6cb8d427f1b0fefd32e36f
Adiman007 <adiman.bonnefoy@gmail.com>
2024-10-04 15:12:33 +0200

    adding sql file for tests and adding instructions file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Test-Technique-Libheros
+
+- read the "Test Libheros.pdf" file for the prerequies  
+
+- Backend using Express.js
+
+- Frontend using React.js 
+
+- populate.sql is the script to create the tables in PostgreSQL and add fake users for tests purposes
---
+++ populate.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    long_description TEXT,
+    completed BOOLEAN DEFAULT FALSE,
+    end_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Insert fake users
+INSERT INTO users (username, email, password) VALUES
+('user1', 'user1@example.com', 'password1'),
+('user2', 'user2@example.com', 'password2'),
+('user3', 'user3@example.com', 'password3');
+
+-- Insert todos
+INSERT INTO todos (user_id, title, description, long_description, completed, end_date) VALUES
+(1, 'Todo 1 for user 1', 'Description for todo 1', 'Long description for todo 1', FALSE, '2024-12-31'),
+(3, 'Todo 1 for user 3', 'Description for todo 1', 'Long description for todo 1', FALSE, '2024-12-31'),
+(3, 'Todo 2 for user 3', 'Description for todo 2', NULL, TRUE, '2024-12-30');
+
+
+select * from todos;
+select * from users;
+
